daf29f5cad4ebdf04c503a4327061a9aa4fda84c
RaidenCHEN <raidenchen@hotmail.com>
2022-02-24 11:49:09 +0800

    Revert "上传代码"
    This reverts commit 24a0fb35975041d23172eb9c71c3e55ce86d24d2.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Pokemon
- OOAD_project
--- cs_code/BagScript/Bag.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Bag", menuName = "Inventory/New Bag")]
-public class Bag : ScriptableObject
-{
-    public List<Item> itemList = new List<Item>();
-}
